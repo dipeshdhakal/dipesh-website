c67cd210db05605572382b9256e684f55158551a
Dipesh Dhakal <dipesh@frollo.us>
2022-04-17 15:54:45 +1000

    WIP

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -21,7 +21,7 @@ function Home() {
               </h1>
 
               <h1 className="heading-name">
-                I'M
+                I am
                 <strong className="main-name"> DIPESH DHAKAL</strong>
               </h1>
 
