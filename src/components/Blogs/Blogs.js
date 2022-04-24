797a47b2f414c2a54a29ea6d491646d9d39a03f7
Dipesh Dhakal <dipesh@frollo.us>
2022-04-24 21:08:34 +1000

    Fix blogs

--- src/components/Blogs/Blogs.js
+++ src/components/Blogs/Blogs.js
@@ -1,19 +1,21 @@
 import { Container } from "react-bootstrap";
 import Particle from "../Particle";
 
-
 function Blogs() {
 
   return (
     <div>
-      <Container fluid className="resume-section">
+      <Container fluid className="blog-section">
         <Particle />
         <h1 className="project-heading">
           My <strong className="purple">Blogs </strong>
         </h1>
-        <p style={{ color: "white" }}>
-          Hmmmm, my blogs are very private to me. Not sure i want to share it. Let me think...
+        <p style={{ color: "white", height:"100%" }}>
+          WIP...
         </p>
+        <div className="blog-content">
+          
+        </div>
       </Container>
     </div>
   );
